2a6be8ec7e84b758143b4cb61e8cdc84b3b27b56
Recessive <aa.mindustry@gmail.com>
2020-09-05 17:16:45 +1000

    Added some barebones functionality

--- src/main/java/campaign/CampaignPlugin.java
+++ src/main/java/campaign/CampaignPlugin.java
@@ -4,61 +4,116 @@ import arc.*;
 import arc.util.*;
 import mindustry.*;
 import mindustry.content.*;
+import mindustry.core.GameState;
 import mindustry.entities.type.*;
+import mindustry.game.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.plugin.Plugin;
+import mindustry.type.ItemStack;
+
+import java.util.prefs.Preferences;
+
+import static mindustry.Vars.*;
+import static mindustry.Vars.netServer;
 
 public class CampaignPlugin extends Plugin{
 
-    //register event handlers and create variables in the constructor
-    public CampaignPlugin(){
-        //listen for a block selection event
-        Events.on(BuildSelectEvent.class, event -> {
-            if(!event.breaking && event.builder != null && event.builder.buildRequest() != null && event.builder.buildRequest().block == Blocks.thoriumReactor && event.builder instanceof Player){
-                //send a message to everyone saying that this player has begun building a reactor
-                Call.sendMessage("[scarlet]ALERT![] " + ((Player)event.builder).name + " has begun building a reactor at " + event.tile.x + ", " + event.tile.y);
+    private Preferences prefs;
+    private int launchWave;
+    private int techLevel;
+    private int wave = 0;
+    private boolean finalWave = false;
+
+    private final Rules rules = new Rules();
+
+    @Override
+    public void init(){
+
+        init_rules();
+
+        Events.on(EventType.WaveEvent.class, event ->{
+            wave ++;
+            if(wave == launchWave){
+                Call.sendMessage("[scarlet]FINAL WAVE!\n[accent]Survive this to progress to the next map");
+                finalWave = true;
             }
         });
-    }
 
-    //register commands that run on the server
-    @Override
-    public void registerServerCommands(CommandHandler handler){
-        handler.register("reactors", "List all thorium reactors in the map.", args -> {
-            for(int x = 0; x < Vars.world.width(); x++){
-                for(int y = 0; y < Vars.world.height(); y++){
-                    //loop through and log all found reactors
-                    if(Vars.world.tile(x, y).block() == Blocks.thoriumReactor){
-                        Log.info("Reactor at {0}, {1}", x, y);
-                    }
+        Events.on(EventType.UnitDestroyEvent.class, event ->{
+            boolean endgame = true;
+            for(Unit unit : unitGroup.all()){
+                if(unit.getTeam() == Team.crux){
+                    endgame = false;
+                    break;
                 }
             }
+
+
         });
     }
 
-    //register commands that player can invoke in-game
+
     @Override
-    public void registerClientCommands(CommandHandler handler){
-
-        //register a simple reply command
-        handler.<Player>register("reply", "<text...>", "A simple ping command that echoes a player's text.", (args, player) -> {
-            player.sendMessage("You said: [accent] " + args[0]);
-        });
-
-        //register a whisper command which can be used to send other players messages
-        handler.<Player>register("whisper", "<player> <text...>", "Whisper text to another player.", (args, player) -> {
-            //find player by name
-            Player other = Vars.playerGroup.find(p -> p.name.equalsIgnoreCase(args[0]));
-
-            //give error message with scarlet-colored text if player isn't found
-            if(other == null){
-                player.sendMessage("[scarlet]No player by that name found!");
+    public void registerServerCommands(CommandHandler handler) {
+        handler.register("campaign", "Begin hosting the Campaign gamemode.", args -> {
+            if (!Vars.state.is(GameState.State.menu)) {
+                Log.err("Stop the server first.");
                 return;
             }
 
-            //send the other player a message, using [lightgray] for gray text color and [] to reset color
-            other.sendMessage("[lightgray](whisper) " + player.name + ":[] " + args[1]);
+            prefs = Preferences.userRoot().node(this.getClass().getName());
+            int currMap = prefs.getInt("mapchoice",0);
+            prefs.putInt("mapchoice", 0); // This is just backup so the server reverts to first map if a map crashes
+
+            mindustry.maps.Map map = maps.customMaps().get(currMap);
+            world.loadMap(map);
+            String[] values = world.getMap().description().split(",");
+            launchWave = Integer.parseInt(values[0]);
+            techLevel = Integer.parseInt(values[1]);
+            rules.spawns = world.getMap().rules().spawns;
+
+            Log.info("Map loaded.");
+
+            // Create cells objects
+
+            state.rules = rules.copy();
+            logic.play();
+
+            netServer.openServer();
+
+        });
+    }
+
+    void init_rules(){
+        rules.waitForWaveToEnd = false;
+        rules.respawnTime = 0;
+        rules.enemyCheat = true;
+        rules.waves = true;
+        rules.waveSpacing = 20 * 60;
+        rules.launchWaveMultiplier = 2;
+        rules.bossWaveMultiplier = 3;
+
+    }
+
+    void endgame(boolean win){
+        if(win){
+            Call.onInfoMessage("[green]Congratulations! You survived.");
+        }else{
+            Call.onInfoMessage("[scarlet]Bad luck! You died.");
+        }
+
+        Time.runTask(60f * 10f, () -> {
+
+            for(Player player : playerGroup.all()) {
+                Call.onConnect(player.con, "aamindustry.play.ai", 6567);
+            }
+
+
+            // I shouldn't need this, all players should be gone since I connected them to hub
+            // netServer.kickAll(KickReason.serverRestarting);
+            Log.info("Game ended successfully.");
+            Time.runTask(60f*2, () -> System.exit(2));
         });
     }
 }
